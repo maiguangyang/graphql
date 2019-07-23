b75dcbbb90e3d3433709481c81b09d94506c31b4
maiguangyang <maiguangyang@163.com>
2019-07-23 16:45:28 +0800

    修改数据库连接地址

--- cmd/init.go
+++ cmd/init.go
@@ -244,7 +244,7 @@ func createMakeFile() error {
 	go run github.com/maiguangyang/graphql
 
 run:
-	DATABASE_URL=mysql://'root:123456@tcp(192.168.33.10:3306)/graphql?charset=utf8mb4&parseTime=True&loc=Local' go run *.go
+	DATABASE_URL=mysql://'root:123456@tcp(localhost:3306)/graphql?charset=utf8mb4&parseTime=True&loc=Local' go run *.go
 
 voyager:
 	docker run --rm -v ` + "`" + `pwd` + "`" + `/gen/schema.graphql:/app/schema.graphql -p 8080:80 graphql/voyager
