82f53960415863437552197588ff469b40f5e719
maiguangyang <maiguangyang@163.com>
2019-08-07 16:33:38 +0800

    整理文件

---
+++ templates/dummy-model.go
@@ -0,0 +1,15 @@
+package templates
+
+var DummyModel = `type User {
+	email: String @column(gorm: "type:varchar(64) comment '用户邮箱地址';default:null;") @validator(required: "true", tye: "email")
+	firstName: String
+	lastName: String
+	tasks: [Task!]! @relationship(inverse:"assignee")
+}
+type Task {
+	title: String
+	completed: Boolean
+	dueDate: Time
+	assignee: User @relationship(inverse:"tasks")
+}
+`
---
+++ templates/makefile.go
@@ -0,0 +1,9 @@
+package templates
+
+var Makefile = `generate:
+	go run github.com/maiguangyang/graphql
+run:
+	DATABASE_URL=mysql://'root:123456@tcp(localhost:3306)/graphql?charset=utf8mb4&parseTime=True&loc=Local' go run *.go
+voyager:
+	docker run --rm -v ` + "`" + `pwd` + "`" + `/gen/schema.graphql:/app/schema.graphql -p 8080:80 graphql/voyager
+`
